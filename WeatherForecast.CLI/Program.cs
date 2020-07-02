56c27c9f030063a66e38ecf76bbce81c7a79695d
wiechecdawid <wiechec.dawid@gmail.com>
2020-07-02 02:09:17 +0200

    Initial Commit

---
+++ WeatherForecast.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherForecast.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WeatherForecast.Lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WeatherForecast.Lib
+{
+    public class Class1
+    {
+    }
+}
